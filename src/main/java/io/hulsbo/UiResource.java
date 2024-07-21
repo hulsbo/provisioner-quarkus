f6f07d290e5c4f78091baa01ef3b21f8d3072eeb
hulsbo <oskar.huledal@simonsoft.se>
2024-07-21 15:55:53 +0200

    UiResource for generic elements. i.e. list.html

--- src/main/java/io/hulsbo/UiResource.java
+++ src/main/java/io/hulsbo/UiResource.java
@@ -1,17 +1,146 @@
 package io.hulsbo;
-
+import io.hulsbo.model.Adventure;
+import io.hulsbo.model.BaseClass;
+import io.hulsbo.model.Manager;
+import io.hulsbo.model.Meal;
+import io.hulsbo.util.model.SafeID;
+import io.quarkus.qute.Location;
+import io.quarkus.qute.Template;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static io.hulsbo.AdventureResource.createComponentInstance;
+
 @Path("/ui")
 public class UiResource {
+
+    @Inject
+    @Location("list.html")
+    Template listTemplate;
 
     @GET
     @Path("/clear")
     @Produces(MediaType.TEXT_HTML)
     public Response returnModal() {
         return Response.ok().build();
+    }
+
+    @GET
+    @Path("/list/adventures")
+    @Produces(MediaType.TEXT_HTML)
+    public Response getAdventureList() {
+
+        List<Adventure> adventures = Manager.getAllAdventures();
+
+        Map<String, String> actions = Map.of(
+                "add", "/adventures/",
+                "edit", "/adventures/{id}",
+                "remove", "/adventures/{id}"
+        );
+
+        // Render in Qute
+        String renderedHtml = listTemplate.data(
+                "actions", actions,
+                "items", adventures,
+                "type", "adventure"
+        ).render();
+
+        // Create component instance
+        String componentInstance = createComponentInstance(renderedHtml, listTemplate);
+
+        return Response.ok(componentInstance).build();
+    }
+
+    @GET
+    @Path("/list/{parentId}/{type}")
+    @Produces(MediaType.TEXT_HTML)
+    public Response getList(
+            @PathParam("parentId") String parentId,
+            @PathParam("type") String type
+    ) {
+
+        SafeID id = SafeID.fromString(parentId);
+
+        if (id == null) {
+            throw new WebApplicationException("parentId "+ parentId + " was not a valid SafeID", Response.Status.NOT_FOUND);
+        }
+
+        BaseClass parent = Manager.getBaseClass(id);
+
+        if (parent == null) {
+            throw new WebApplicationException("Parent object could not be found", Response.Status.NOT_FOUND);
+        }
+
+        if (type == null) {
+            throw new WebApplicationException("No type parameter in path.", Response.Status.NOT_FOUND);
+        }
+
+        List<?> items;
+        List<String> columns;
+        Map<String, String> actions;
+
+        switch (type.toLowerCase()) {
+
+            case "crewmember":
+                if (!(parent instanceof Adventure)) {
+                    throw new WebApplicationException("Parent must be an Adventure for crewmember list", Response.Status.BAD_REQUEST);
+                }
+
+                items = ((Adventure) parent).getAllCrewMembers();
+                actions = Map.of(
+                        "add", "/adventures/" + parentId + "/crewmember",
+                        "remove", "/adventures/" + parentId + "/crewmember/{childId}"
+                );
+                break;
+
+            case "meal":
+                if (!(parent instanceof Adventure)) {
+                    throw new WebApplicationException("Parent must be an Adventure for meal list", Response.Status.BAD_REQUEST);
+                }
+
+                items = parent.getAllChildren();
+                actions = Map.of(
+                        "add", "/adventures/" + parentId + "/meal",
+                        "edit", "/adventures/" + parentId + "/meal/{childId}",
+                        "remove", "/adventures/" + parentId + "/meal/{childId}"
+                );
+                break;
+
+            case "ingredient":
+                if (!(parent instanceof Meal)) {
+                    throw new WebApplicationException("Parent must be a Meal for ingredient list", Response.Status.BAD_REQUEST);
+                }
+
+                items = parent.getAllChildren();
+                actions = Map.of(
+                        "add", "/meal/" + parentId + "/ingredient",
+                        "edit", "/meal/" + parentId + "/ingredient/{childId}",
+                        "remove", "/meal/" + parentId + "/ingredient/{childId}"
+                );
+                break;
+
+            default:
+                throw new WebApplicationException("Invalid type parameter", Response.Status.BAD_REQUEST);
+        }
+
+        // Render in Qute
+        String renderedHtml = listTemplate.data(
+                "type", type,
+                "parent", parent,
+                "actions", actions,
+                "items", items
+        ).render();
+
+        // Create component instance
+        String componentInstance = createComponentInstance(renderedHtml, listTemplate);
+
+        return Response.ok(componentInstance).build();
     }
 
 }
