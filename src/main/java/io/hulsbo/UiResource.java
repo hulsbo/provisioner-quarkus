327350f71fde84c171426dbcbdb8e3cf4389fbc8
hulsbo <oskar.huledal@simonsoft.se>
2024-07-25 00:13:55 +0200

    remove unneeded imports

--- src/main/java/io/hulsbo/UiResource.java
+++ src/main/java/io/hulsbo/UiResource.java
@@ -10,8 +10,6 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
